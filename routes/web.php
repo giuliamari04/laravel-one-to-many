30bee85058bd3c814e8aeacacc16751bd334034d
giuliamari04 <giuliamari04@gmail.com>
2024-01-18 10:44:34 +0100

    rotte

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\CategoryController;
 
 use Illuminate\Support\Facades\Route;
 
@@ -23,7 +24,12 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-    Route::resource('posts', PostController::class);
+    Route::resource('posts', PostController::class)->parameters([
+        'posts' => 'post:slug'
+    ]);
+    Route::resource('categories', CategoryController::class)->parameters([
+        'categories' => 'category:slug'
+    ]);
 });
 
 
